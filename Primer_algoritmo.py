194c4b1cb580921f66e3c80ac8f946888d818b30
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-08-23 17:34:15 -0500

    algoritmo v1

---
+++ Primer_algoritmo.py
@@ -0,0 +1,5 @@
+# alcular el area del terreno
+B = float(input("Escribe la base del triangulo: "))
+H = float(input("Escribe la altura del triangulo: "))
+A = (B * H) / 2
+print ("EL area del triangulo es: ",(A))
