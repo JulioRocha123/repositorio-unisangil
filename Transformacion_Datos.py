11ff33494aa3cf8513ba1efe50a8976e142387f8
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-08-23 16:37:52 -0500

    Transformacion_Datos 1

---
+++ Transformacion_Datos.py
@@ -0,0 +1,5 @@
+#capturar informacion
+nombre = str(input("Digite su nombre completo: "))
+cedula = int(input("digite su tipo de cedula"))
+print("su nombre es: ", (nombre))
+print("su cedula es: ", (cedula))
