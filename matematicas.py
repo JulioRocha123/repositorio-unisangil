769edb0c5891123f069f5ead8ca35965e117bc7c
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-08-30 16:29:25 -0500

    matemativas v2

--- matematicas.py
+++ matematicas.py
@@ -7,5 +7,11 @@ print (ejemplo1)
 ejemplo2 = (4+3)-((3*2)+1)
 print (ejemplo2)
 #ejmeplo 3
-ejemplo3 = 2*(3+(5(9/3)))
+ejemplo3 = 2*3+5*(9/3)
 print (ejemplo3)
+#ejemplo 4
+ejemplo4 = 7+3*(1+5*(2*(9-7)+4)+2)
+print (ejemplo4)
+#ejemplo5
+ejemplo5 = (15/3)*(7+(68-15*33+((45*45)/5)/3)/2)+19
+print (ejemplo5)
