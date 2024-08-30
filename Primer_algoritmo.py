5e5fa9cd939e1eab2ae58b9979a8d0d2e9c576ae
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-08-30 08:18:25 -0500

    Calculdora de terrenos triangulares

--- Primer_algoritmo.py
+++ Primer_algoritmo.py
@@ -1,5 +1,8 @@
-# alcular el area del terreno
+# se le da la bienvenida al usuario 
+print ("Bienvenido a la calculadora de terrenos con forma de triangulos")
+# el ususario ingresa los datos de la base y la altura del terreno en forma triangular
 B = float(input("Escribe la base del triangulo: "))
 H = float(input("Escribe la altura del triangulo: "))
+#se hace la operacion para hallar el area del terreno en forma triangular
 A = (B * H) / 2
 print ("EL area del triangulo es: ",(A))
