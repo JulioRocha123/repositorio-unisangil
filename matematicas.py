141a81c0ff508e85be0c3bf62b76bbca5bf0bbf8
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-08-30 17:10:16 -0500

    matematicas v2.1

--- matematicas.py
+++ matematicas.py
@@ -15,3 +15,6 @@ print (ejemplo4)
 #ejemplo5
 ejemplo5 = (15/3)*(7+(68-15*33+((45*45)/5)/3)/2)+19
 print (ejemplo5)
+#ejemplo6
+ejemplo6 = (16/4 *((((4+8*2)/4- (3**2) + math.sqrt(4))/2) * math.sqrt(25) )-3)+16
+print (ejemplo6)
