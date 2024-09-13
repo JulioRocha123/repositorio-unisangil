9e562d568787f55bca6999a38cba703ca166b0ad
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-09-13 16:14:29 -0500

    clasificar edad V1

---
+++ EstructutasIF.py
@@ -0,0 +1,30 @@
+#variables
+edad = float(input("Ingrese su edad: "))
+if (edad <= 0):
+    print("usted es un preinfante ")
+elif (edad > 0 and edad <= 5):
+    print ("usted es un primera infante")
+elif (edad >= 6 and edad <= 11):
+    print ("usted es un infante")
+elif (edad >= 12 and edad <= 17):
+    print ("Usted es un adolecesnte")
+elif (edad >= 18 and edad <= 28):
+    print ("usted es joven")
+elif (edad >= 29 and edad <= 59):
+    print ("usted esta adulto")
+elif (edad >= 60 and edad <= 200):
+    print ("sumerce esta viejito, cuidese")
+else:
+    print ("sumerce ya es un dinosaurio jijiji <3")
+
+"""
+num1 = 2
+if (num1 == 10):
+    print ("true")
+elif num1 == 1:
+    print ("true")
+elif num1 == 2:
+    print ("true")
+else:
+    print ("false")
+"""
