286cea52b3f82afedadaef50a95a045b943e22df
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-09-13 15:29:51 -0500

    programadores logicos v1.2

--- Programadores_logicoa.py
+++ Programadores_logicoa.py
@@ -1,2 +1,25 @@
-import math
-print ("operadores logicos")
+#variables
+num1 = 7
+num2 = 8
+#operador and
+print ("AND")
+print (num1 == num2 and num1 <= num2)
+print (num1 == num2 and num1 > num2)
+print (num1 < num2 and num1 > num2)
+print (num1 > num2 and num1 < num2)
+#ejemplo 1
+"""
+ususario = str(input("digite el ususario: "))
+contraseña = str(input("digite la contraseña: "))
+print (ususario == "Julio" and contraseña == "1034780288")
+"""
+#operador or
+print ("OR")
+print (num1 == num2 or num1 <= num2)
+print (num1 == num2 or num1 > num2)
+print (num1 < num2 or num1 > num2)
+print (num1 > num2 or num1 < num2)
+#operador not
+print ("NOT")
+print (not True)
+print (not False)
